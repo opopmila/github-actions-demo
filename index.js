2e788210c615b46472f55c6bb1fdfbca96c94e44
opopmila <137287639+opopmila@users.noreply.github.com>
2025-02-22 12:14:44 +0300

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-function isEven(day) {
+function isEven() {
   const date = new Date();
   const currentDay = date.getDate();
   return currentDay % 2 === 0;
 }
 
 console.log(`Сегодня ${isEven() ? 'чётный' : 'нечётный'} день.`);
+
+module.exports = { isEven };
